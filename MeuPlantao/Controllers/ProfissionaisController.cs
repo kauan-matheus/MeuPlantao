7b25d7f139273c5736505d1a79a9e89387b87b2a
dolphin4748 <matheusluciocx5726@gmail.com>
2026-02-27 22:21:06 -0300

    aaaaa

--- MeuPlantao/Controllers/ProfissionaisController.cs
+++ MeuPlantao/Controllers/ProfissionaisController.cs
@@ -1,6 +1,23 @@
-﻿namespace MeuPlantao.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using MeuPlantao.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace MeuPlantao.Controllers;
 
 public class ProfissionaisController
 {
+    private readonly AppDbContext _appDbContext;
+
+    public ProfissionaisController(AppDbContext appDbContext)
+    {
+        _appDbContext = appDbContext;
+    }
+
+    [HttpGet("profissionais")]
+    public async Task<IActionResult> GetProfissionais()
+    {
+        var resultado = await _appDbContext.Profissionais.ToListAsync();
+        return Ok(resultado);
+    }
     
 }
